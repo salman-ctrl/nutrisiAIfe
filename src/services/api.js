961808d6251609f6361d8207dff6d6716375b308
salman-ctrl <zirmanvictory@gmail.com>
2025-12-14 01:34:53 +0700

    perbaiki api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const PRODUCTION_URL = 'https://nutrisiaibe-4.onrender.com/api';
+
+const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: isLocal ? 'http://localhost:5000/api' : PRODUCTION_URL,
 });
 
 api.interceptors.request.use((config) => {
@@ -16,11 +20,8 @@ api.interceptors.response.use(
     (response) => response,
     (error) => {
         if (error.response && (error.response.status === 401 || error.response.status === 400)) {
-            
             localStorage.removeItem('token');
             localStorage.removeItem('user');
-            
-           
             if (window.location.pathname !== '/login' && window.location.pathname !== '/register') {
                 window.location.href = '/login';
             }
